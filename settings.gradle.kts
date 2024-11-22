3780c7f18e13122fcc084367f2aa0cdee45f3315
Maria E. Martin Fuentes <me.martinfuentes@gmail.com>
2024-11-22 21:15:34 +0100

    design main activity

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AE1_Calculadadora_salario_Fuentes"
+include(":app")
+ 
